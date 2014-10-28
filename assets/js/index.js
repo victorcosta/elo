8685d2a1b9115d1325d6079d6ad8587ee4aade96
Victor Costa <victorcostaguitar@gmail.com>
2014-10-27 22:51:17 -0300

    corrigindo url

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener('deviceready', onDeviceReady, true);
 document.addEventListener("offline", onOffLine, false);
 
 function onDeviceReady() {
-	var ref = window.open(encodeURI('http://elogold-acceptance.herokuapp.com/users/sing_in'), '_self', 'location=no');
+	var ref = window.open(encodeURI('http://elogold-acceptance.herokuapp.com/users/sign_in'), '_self', 'location=no');
 	// checkConection();
 }
 function onOffLine() {

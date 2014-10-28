534e0b019d0671b271c8bb5f8b47594721a6a323
Victor Costa <victorcostaguitar@gmail.com>
2014-10-27 22:46:38 -0300

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('deviceready', onDeviceReady, true);
+document.addEventListener("offline", onOffLine, false);
+
+function onDeviceReady() {
+	var ref = window.open(encodeURI('http://elogold-acceptance.herokuapp.com/users/sing_in'), '_self', 'location=no');
+	// checkConection();
+}
+function onOffLine() {
+	navigator.notification.vibrate(100);
+	navigator.notification.alert("Você está sem conexão com internet",closeApp,"Alerta",'OK!');
+}
+function closeApp(){
+	if (navigator.app) {
+		navigator.app.exitApp();
+	}else if (navigator.device) {
+		navigator.device.exitApp();
+	}
+}
